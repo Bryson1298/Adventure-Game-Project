2a291c40b0a91585823ce28eccd4bf9a88e9d09f
Bryson1298 <bubbacorcoran5@gmail.com>
2025-03-22 00:00:52 -0600

    Update game.py

--- game.py
+++ game.py
@@ -1,6 +1,15 @@
 from gamefunctions import *
 
 def python_game():
+    """
+    Starts the main game loop.
+
+    Calls the `gameloop()` function from the `gamefunctions` module to initiate the game.
+
+    Returns:
+        None
+    """
     gameloop()
+
 
 python_game()
